d644a46664e2ec88896b8f467548be98d75b0dec
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-11-05 20:55:58 -0600

    Actualizando propiedades

--- js/index.js
+++ js/index.js
@@ -1,26 +1,29 @@
 
 fetch("https://fakestoreapi.com/products")
     .then (response =>  response.json())
-    .then (ropa => {
+    .then (products => {
         //no lleva elementos antes, por eso va vacia
         let card = '';
 
-        for (let db of ropa){
+        for (let db of products){
             card += 
             ` 
             <div class="row mt-5">
-                <div class="col">
+                <div class="col-5">
                     <div class="card" style="width: 18rem;">
                         <img src="${db.image}" class="card-img-top">
                         <div class="card-body">
                         <h5 class="card-title">"${db.title}"</h5>
                     </div>
-                </div>
-                
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Item: ${db.id}</li>
+
+                    <ul class="list-group ">
+                        <li class="list-group-item">Description: ${db.description}</li>
+                        <li class="list-group-item">Price: $ ${db.price}</li>
+                        <li class="list-group-item">Category: ${db.category}</li>
                     </ul>
-                
+
+                </div>             
+                                  
             </div>
            `
         }
